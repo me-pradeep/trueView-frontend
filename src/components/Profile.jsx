61d3db85699b4dae0530fc9b2158b6ae11637329
Pradeep Yadav <pradeepyd708@gmail.com>
2025-01-18 14:55:15 +0530

    ui changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -26,7 +26,7 @@ function Profile({ username }) {
           numOfRatingsReceived: userData.numOfRatingsReceived,
           overallRating: userData.overallRating,
           email: userData.email,
-          ratingCount:userData.ratingCount,
+          ratingCount: userData.ratingCount,
         });
 
         setLoading(false);
@@ -37,19 +37,27 @@ function Profile({ username }) {
 
     fetchUserData();
   }, [username]);
-  
+
   if (loading) {
     return (
-      <div className="w-[80%] max-lg:w-[80%] max-md:w-[100%] bg-white rounded-lg flex gap-4 p-4">
-        <Skeleton
-          variant="circular"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
-        <div className="w-[40%] max-md:w-[80%] flex gap-2 flex-col">
-          <Skeleton variant="text" width="40%" height={32} />
-          <Skeleton variant="text" width="80%" height={40} />
+      <div className="w-[80%] bg-blue-500 rounded-lg flex justify-around gap-4 p-4 max-lg:flex-col max-lg:w-full">
+        <div className="flex w-[50%] gap-4 max-md:w-full items-center justify-center">
+          <Skeleton
+            variant="circular"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <div className="w-[60%] max-md:w-[80%] flex gap-2 flex-col">
+            <Skeleton variant="text" width="60%" height={32} />
+            <Skeleton variant="text" width="90%" height={40} />
+          </div>
+        </div>
+        <div className=" w-[50%] flex max-lg:w-full justify-center gap-4 border-l-2 max-lg:border-l-0 max-lg:border-t-2 max-lg:pt-2 pl-4 max-md:pl-2 border-white items-center max-lg:justify-center">
+          <Skeleton variant="text" width="80%" height={60} />
+          <Skeleton variant="text" width="80%" height={60} />
+          <Skeleton variant="text" width="80%" height={60} />
+          <Skeleton variant="text" width="80%" height={60} />
         </div>
       </div>
     );
@@ -70,8 +78,8 @@ function Profile({ username }) {
   } = user;
 
   return (
-    <div className="w-[80%] bg-white rounded-lg flex gap-4 p-4 max-lg:flex-col max-lg:w-full">
-      <div className="flex gap-4 max-md:w-full">
+    <div className="w-[80%] bg-blue-500 rounded-lg flex justify-around gap-4 p-4 max-lg:flex-col max-lg:w-full">
+      <div className="flex gap-4 max-md:w-full items-center">
         <div>
           <Image
             priority
@@ -83,28 +91,28 @@ function Profile({ username }) {
           />
         </div>
         <div className="w-[90%] flex gap-2 flex-col">
-          <div className="font-bold text-xl text-slate-400 w-fit">
+          <div className="font-bold text-xl text-white w-fit">
             {displayUsername}
           </div>
-          <div className="text-slate-500">{bio}</div>
+          <div className="text-white">{bio}</div>
         </div>
       </div>
-      <div className=" flex max-lg:w-full justify-center gap-4 border-l-2 max-lg:border-l-0 max-lg:border-t-2 max-lg:pt-2 pl-2 border-slate-400 items-center max-lg:justify-center">
+      <div className="flex max-lg:w-full text-white justify-center gap-4 border-l-2 max-lg:border-l-0 max-lg:border-t-2 max-lg:pt-2 pl-4 max-md:pl-2 border-white items-center max-lg:justify-center">
         <div className="flex flex-col items-center font-semibold">
           {numOfRatingsGiven}
-          <div className="text-center text-slate-400">Ratings Given</div>
+          <div className="text-center ">Ratings Given</div>
         </div>
         <div className="flex flex-col items-center font-semibold">
           {numOfRatingsReceived}
-          <div className="text-center text-slate-400">Ratings Received</div>
+          <div className="text-center">Ratings Received</div>
         </div>
         <div className="flex flex-col items-center font-semibold">
           {overallRating}/10
-          <div className="text-center text-slate-400">Average Rating</div>
+          <div className="text-center">Average Rating</div>
         </div>
         <div className="flex flex-col items-center font-semibold">
           {ratingCount}
-          <div className="text-center text-slate-400">Rating Count</div>
+          <div className="text-center">Rating Count</div>
         </div>
       </div>
     </div>
