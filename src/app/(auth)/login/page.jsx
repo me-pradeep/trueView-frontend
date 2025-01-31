db7c1c37563f5115538dbca6599a844842992133
Pradeep Yadav <pradeepyd708@gmail.com>
2025-01-31 18:31:36 +0530

    made few changes for google verification

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import Google from "@/components/google";
+import Link from "next/link";
 
 function page() {
   return (
@@ -19,6 +20,10 @@ function page() {
         <span className="text-red-400">Anonymously</span>, at Any Time.
       </div>
       <Google />
+      <div className="flex gap-3">
+        <Link href="/privacypolicy" className="text-blue-600">Privacy Policy</Link>
+        <Link href="/termsofservice" className="text-blue-600">Terms of Service</Link>
+      </div>
     </>
   );
 }
